a9bd8eadde40eb4d8bb2b436c6cf1140cb9eb5a0
Zexi Liu <zexi.liu@uisee.com>
2023-11-29 17:00:59 +0800

    New: init commit

---
+++ main.py
@@ -0,0 +1,20 @@
+'''
+Author: Zexi Liu
+Date: 2023-11-28 17:11:02
+LastEditors: Zexi Liu
+LastEditTime: 2023-11-29 17:00:17
+FilePath: /seg_data_tools/main.py
+Description:
+
+Copyright (c) 2023 by Uisee, All Rights Reserved.
+'''
+
+import os
+import numpy as np
+
+INPUT_BIN_FOLDER = ''
+INPUT_LABEL_FOLDER = ''
+INPUT_LIDAR_STATE = ''
+
+
+if __name__ == '__main__':
